1caeed5f2f329a9e8594e6d1fce3bf24d005c400
abichan99911111 <shinei-a@weseek.co.jp>
2025-02-23 14:29:28 +0900

    binding pry 導入

--- rails-api/spec/models/user_spec.rb
+++ rails-api/spec/models/user_spec.rb
@@ -20,7 +20,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
-
+  
   describe 'validations' do
     it 'is valid with valid attributes' do
       expect(user).to be_valid
