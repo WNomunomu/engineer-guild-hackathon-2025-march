c272e410b18d2f370a6ff53ddcd8917c134282fb
abichan99911111 <shinei-a@weseek.co.jp>
2025-02-23 14:27:42 +0900

    devise model spec

---
+++ rails-api/spec/models/user_spec.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { create(:user) }
+
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      expect(user).to be_valid
+    end
+
+    it 'is not valid without an email' do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'is not valid without a password' do
+      user.password = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'is not valid with a duplicate email' do
+      duplicate_user = build(:user, email: user.email)
+      expect(duplicate_user).to_not be_valid
+    end
+  end
+
+  describe 'authentication' do
+    it 'authenticates with correct password' do
+      expect(user.valid_password?('password')).to be_truthy
+    end
+
+    it 'does not authenticate with incorrect password' do
+      expect(user.valid_password?('wrong_password')).to be_falsey
+    end
+  end
+end
