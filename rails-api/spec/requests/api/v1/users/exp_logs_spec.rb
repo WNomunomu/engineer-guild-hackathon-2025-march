8d4de70558d1fc81091de142645143b266692939
NaokiHigashi28 <naoki-h@weseek.co.jp>
2025-02-28 01:49:48 +0000

    update spec

--- rails-api/spec/requests/api/v1/users/exp_logs_spec.rb
+++ rails-api/spec/requests/api/v1/users/exp_logs_spec.rb
@@ -9,13 +9,42 @@ RSpec.describe "Api::V1::User::ExpLogs", type: :request do
         header["client"] = res["client"]
         header["access-token"] = res["access-token"]
         header["uid"] = res["uid"]
+      end
+      let(:user) { User.find_by(email: "a@a.com") }
+      let(:category1) { create :category }
+      let(:category2) { create :category }
+      let(:book1) do
+        book = create(:book)
+        book.categories << category1
+        book
+      end
+      let(:book2) do
+        book = create(:book)
+        book.categories << category2
+        book
+      end
+      let!(:reading_log1) { create :reading_log, book: book1, user: user , pages_read: 100}
+      let!(:reading_log2) { create :reading_log, book: book1, user: user , pages_read: 200}
+      let!(:reading_log3) { create :reading_log, book: book2, user: user , pages_read: 200}
 
-        User.find_by(email: "a@a.com").exp_logs.create!(exp_points: 10, earned_at: Date.today, category: create(:category))
+      let(:expected_body) do
+        [
+          {
+            "category_id"   => category1.id,
+            "category_name" => category1.category,
+            "pages_read"    => 300
+          },
+          {
+            "category_id"   => category2.id,
+            "category_name" => category2.category,
+            "pages_read"    => 200
+          },
+        ]
       end
       it "進捗が取得できること" do
         get "api/v1/users/exp_logs", headers: header
         expect(response).to have_http_status(200)
-        expect(response.body).to eq(User.find_by(email: "a@a.com").exp_logs.to_json)
+        expect(JSON.parse(response.body)).to eq(expected_body)
       end
     end
 
