6273387884f3d3b552292f82b376c1d04a3ae000
WNomunomu <kohsei.nmr@gmail.com>
2025-03-01 18:08:34 +0000

    create http methods

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getAuthHeaders } from "@/utils/auth-utils";
 
 const API_BASE_URL = "http://localhost:3001/api/v1";
 
@@ -9,3 +10,43 @@ export const apiClient = axios.create({
   },
   withCredentials: true,
 });
+
+export const apiV1Get = async (url: string) => {
+  const config = getAuthHeaders();
+  try {
+    const response = await apiClient.get(url, { headers: config });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.errors);
+  }
+};
+
+export const apiV1Post = async (url: string, data?: any) => {
+  const config = getAuthHeaders();
+  try {
+    const response = await apiClient.post(url, data, { headers: config });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.errors);
+  }
+};
+
+export const apiV1Put = async (url: string, data?: any) => {
+  const config = getAuthHeaders();
+  try {
+    const response = await apiClient.put(url, data, { headers: config });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.errors);
+  }
+};
+
+export const apiV1Delete = async (url: string) => {
+  const config = getAuthHeaders();
+  try {
+    const response = await apiClient.delete(url, { headers: config });
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.errors);
+  }
+};
