5ca534d05b43b4a383848178258b63053986d0ed
aoshi2025s <aoshi2025s@gmail.com>
2025-03-03 20:15:11 +0000

    fix resolve

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["ndlsearch.ndl.go.jp", "books.google.com", "placehold.co"], // NDLの書影APIの画像表示のため, google books apiの(ry
+  },
+
+  webpack(config) {
+    config.resolve.alias["@"] = path.join(__dirname, "src");
+    return config;
   },
 };
 
