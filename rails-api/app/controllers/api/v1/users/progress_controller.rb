8550bc7207319f902e4c6ff836ebafa2567cdcfb
NaokiHigashi28 <naoki-h@weseek.co.jp>
2025-03-06 09:00:14 +0000

    update progress controller and spec

--- rails-api/app/controllers/api/v1/users/progress_controller.rb
+++ rails-api/app/controllers/api/v1/users/progress_controller.rb
@@ -5,7 +5,7 @@ module Api
         before_action :authenticate_api_v1_user!
 
         def show
-          total_pages_read = current_api_v1_user.reading_logs.sum(:pages_read)
+          total_pages_read = current_api_v1_user.reading_logs.sum(&:pages_read)
           total_exp = current_api_v1_user.exp_logs.sum(:exp_points)
 
           render json: {
