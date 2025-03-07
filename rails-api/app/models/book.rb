9b4309262c5903bb3736fb90b059fc397acf3733
WNomunomu <kohsei.nmr@gmail.com>
2025-03-07 07:33:19 +0000

    create check_is_completed method

--- rails-api/app/models/book.rb
+++ rails-api/app/models/book.rb
@@ -34,4 +34,27 @@ class Book < ApplicationRecord
   validates :current_pages, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :total_pages, presence: true, numericality: { greater_than: 0 }
   validates :isbn, presence: true, format: { with: /\A\d{10}(\d{3})?\z/, message: "must be a valid 10 or 13 digit ISBN" }
+
+  def check_is_completed
+    if self.total_pages.present?
+
+      all_reading_logs = self.reading_logs
+      
+      read_pages = Array.new(self.total_pages + 1, false)
+      read_pages[0] = true
+      
+      all_reading_logs.each do |log|
+        start_p = [1, log.start_page.to_i].max
+        end_p = [log.end_page.to_i, self.total_pages].min
+        
+        (start_p..end_p).each do |page|
+          read_pages[page] = true
+        end
+      end
+      
+      all_pages_read = read_pages[1..-1].all?
+      
+      self.update(completed: all_pages_read)
+    end
+  end
 end
